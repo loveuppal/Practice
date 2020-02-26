e8e9bfbf1b9f25653cfde4bed08aa45454548003
srikanthpendem <55446776+srikanthpendem@users.noreply.github.com>
2020-02-26 01:17:27 -0500

    Read database of people from txt file

--- DatabaseStruct/src/struct2.c
+++ DatabaseStruct/src/struct2.c
@@ -9,29 +9,37 @@
 #include<string.h>
 
 struct one {
-			    /*Member Variables*/
+				    /*Member Variables*/
 	char firstname[30];
 	char lastname[30];
 	char city[30];
 	};
 
-			    /*Main Function*/
+				    /*Main Function*/
 int main() {
-	struct one dbasepeople; 			//declare database type os struct one.
+	struct one dbasepeople[100]; 					//declare database type os struct one.
 
-	FILE *fpi; 					//creating a file pointer
-	fpi = fopen("hey.txt", "r"); 			//opening and reading the file "hey.txt"
-	char str1[30],str2[30], str3[30]; 		//creating string variables to store
-	fscanf(fpi, "%s %s %s", str1, str2, str3); 	//reading from the text file
+	FILE *fpi; 							//creating a file pointer
+	fpi = fopen("hey.txt", "r"); 					//opening and reading the file "hey.txt"
+	char str1[30],str2[30], str3[30]; 				//creating string variables to store
 
-		strcpy(dbasepeople.firstname, str1);
-		strcpy(dbasepeople.lastname, str2);	//Copying string from text file to struct variable
-		strcpy(dbasepeople.city, str3);
+	int currentline = 0;						// initializing counter to read lines
+	char line[100];							//variable to sing line of string
 
-	printf("Firstname: %s", dbasepeople.firstname);	//Printing the copied string.
-	printf("\nlastname: %s", dbasepeople.lastname);
-	printf("\ncity: %s", dbasepeople.city);
+	while(fgets(line, 100, fpi) != NULL) {				//Reading till end of the line from the file
+	         sscanf(line, "%s %s %s \n",str1,str2,str3);		//Reading strings form the current line into
 
-	fclose(fpi);					//closing the hey.txt file.
+		strcpy(dbasepeople[currentline].firstname, str1);
+		strcpy(dbasepeople[currentline].lastname, str2);	//Copying string from text file to structure variable
+		strcpy(dbasepeople[currentline].city, str3);
+		currentline++;
+	}
+
+	for(int i=0; i<currentline;i++){
+		printf("\nFirstname: %s", dbasepeople[i].firstname);	//Printing the copied string.
+	       	printf("\nlastname: %s", dbasepeople[i].lastname);
+	       	printf("\ncity: %s", dbasepeople[i].city);
+}
+	fclose(fpi);							//closing the hey.txt file.
 
 }
