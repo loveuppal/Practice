48f288f112d25ea9adb4712588b8fbb666a637ed
srikanthpendem <55446776+srikanthpendem@users.noreply.github.com>
2020-02-25 19:17:47 -0500

    Update hey.c

--- ReadFile/hey.c
+++ ReadFile/hey.c
@@ -1,5 +1,5 @@
 /*
- * hey.c
+ * readfile.c
  *
  *  Created on: Feb. 24, 2020
  *      Author: shaan
@@ -8,13 +8,18 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+			/*MAIN FUNCTION*/
 int main() {
-char s[100];
+char s[100];				//creating string variable
 char t[100];
-FILE *fpi;
-fpi = fopen("hey.txt", "r");
-	fscanf(fpi, "%s %s %s", s, t);
-	printf("%s \n", s);
-	printf("%s \n", t );
-	fclose(fpi);
+	
+FILE *fpi;				//creating the file pointer
+fpi = fopen("hey.txt", "r");		//opeining the hey.txt file
+	
+	fscanf(fpi, "%s %s %s", s, t);	//reading the strings in the hey.txt file
+	
+	printf("%s \n", s);		//printing the first string
+	printf("%s \n", t );		//prinintg the second string
+	
+	fclose(fpi);			//closing the file
 }
