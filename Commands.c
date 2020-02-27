2053f3beeec7f6aa394c6e60bb4afc49299a85c5
srikanthpendem <55446776+srikanthpendem@users.noreply.github.com>
2020-02-26 22:29:15 -0500

    Update and rename Commands.linux to Commands.c

--- Commands.c
+++ Commands.c
@@ -1,3 +1,3 @@
-If no dependencies installed fllow
+If no dependencies installed follow
 >> sudo dpkg --configure -a   //checks what dependencies are required
 >> sudo apt-get install -f   //installs the listed missing dependencies
