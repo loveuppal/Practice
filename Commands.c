657ab3782a219de362e1b949cf36ca843e446891
srikanthpendem <55446776+srikanthpendem@users.noreply.github.com>
2020-02-26 22:36:01 -0500

    Update Commands.c

--- Commands.c
+++ Commands.c
@@ -1,3 +1,4 @@
-If no dependencies installed follow
 >> sudo dpkg --configure -a   //checks what dependencies are required
 >> sudo apt-get install -f   //installs the listed missing dependencies
+>> sudo ln -s ../path/../executable_file /usr/local/bin  //To create an application shortcut.
+ 
