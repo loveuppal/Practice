e7d3d362cc9ba454ddaa0d9d3c2adecdf1005107
Srikanth Pendem <55446776+srikanthpendem@users.noreply.github.com>
2020-02-29 00:48:03 -0500

    BlackJack.c in a more beautiful way

--- BlackJack/Blackjack.c
+++ BlackJack/Blackjack.c
@@ -7,61 +7,70 @@
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
+#include<time.h>
 
-	int printRandoms()
+	int RandomNumbers()
 	{
-	    for (int i = 0; i < 1; i++) {
-	        int num = (rand() % (10 - 1 + 1)) + 1;
+	    for (int i = 0; i < 1; i++)								//prints only single number
+	    {
+	        int num = (rand() % (10 - 1 + 1)) + 1;				//prints random numbers from specified limits
 	        return num;
 	    }
-	}
+}
 
-	int playerwon(){return printf("player won");}
-	int dealerwon(){return printf("Dealer won");}
+	int playerwon()
+	{
+		return printf("player won");						//to display that player has won
+}
+
+	int dealerwon()
+	{
+		return printf("Dealer won");						//to display that dealer has won
+}
+
+	int randAdd() {
+		int a = RandomNumbers();
+		int b = RandomNumbers();
+		return a+b;											//Adds two random numbers
+	}
 
 	int main()
 	{
-	    srand(time(0));
-	    char d;						//creating a char variable to question the player
-	    //scanf ("%c ", &d);				//asks if player wants to play
-	    int a = printRandoms(); 				//prints a random number card from deck
-	    int b = printRandoms();
-	    int c = a+b;
-	    int a1 = printRandoms(); 				//prints a random number card from deck
-	    int b1 = printRandoms();
-	    int c1 = a1+b1;
-	    printf(" %d ",c);					//prints what card got from deck
-	    printf(" %d ",c1);
-	    int i=c, k=c1;
-	    int j;
+	    srand(time(0));										//actualize the random numbers on every RUN
+	    int Rnum1, Rnum2, Rnum3, Rnum4;
+
+	    int TwoCardsofPlayer = randAdd(Rnum1, Rnum2);
+	    int TwoCardsofDealer = randAdd(Rnum3, Rnum4);
+
+	    printf(" %d ",TwoCardsofPlayer);					//prints what card got from deck
+	    printf(" %d ",TwoCardsofDealer);
+
+	    int P_Picks=TwoCardsofPlayer, D_Picks=TwoCardsofDealer;	//storing values of first two deal of cards for the further iteration
+
 	    while(1) {
-	    	char d;						//creating a char variable to question the player
-	    	scanf ("%c ", &d);				//asks if player wants to play
-	    		if(d=='y') {				//if the player hits y to start game
-	    		int e = printRandoms(); 		//prints a random number card from deck
-	    		j = printRandoms();
-	    		k = k+j; 				//Dealer's next card
-	    		i = i+e;				//Deal the next card and add to previous card
+	    	char in;
+	    	scanf ("%c ", &in);									//asks if player wants to play
+	    		if(in =='y') {									//player hits y to starts to game
+	    		int ThirdPick_P = RandomNumbers(); 				//Picking the third card of player
+	    		int ThirdPick_D = RandomNumbers();				//picking third card of the dealer
+	    		D_Picks = D_Picks+ThirdPick_D;					//Iterating the Card picks of the Dealer
+	    		P_Picks = P_Picks+ThirdPick_P;					//Iterating the card picks of the player
 
-	    		printf(" %d ",e);			//prints what card got from deck
-	    		printf("%d ",i);				//prints the total. player
+	    		printf(" %d ",ThirdPick_P);						//Prints the Third card picked by Player
+	    		printf("%d ",P_Picks);							//prints the sum of all card iterations
 
-	    		printf(" %d ",j);				//prints what card got from deck
-	    		printf("%d ",k);				//prints the total dealer
-	    		}
-	    		if(d=='n') {
-	    			j = printRandoms();
-	    			k = k+j;
+	    		printf(" %d ",ThirdPick_D);						//Prints the Third card picked by Dealer
+	    		printf("%d ",D_Picks);							//prints the sum of all card iterations
 	    		}
 
-	    		if((i>21 && k<21)||(k==21))
+	    		if((P_Picks>21 && D_Picks<21)||(D_Picks==21))
 	    		return dealerwon();
 
-	    		if((i==21)||(k>21 && i<21))
+	    		if((P_Picks==21)||(D_Picks>21 && P_Picks<21))
 	    		return playerwon();
 
-	    		if(i>21 || k>21)
+	    		if(P_Picks>21 || D_Picks>21)
 	    			return(1);
 
- }										//while loop end
-}											//main ends
+ }															//while loop end
+}															//main ends
