82aaf15a196608bee3659436b59fa6e043759c98
Srikanth Pendem <55446776+srikanthpendem@users.noreply.github.com>
2020-02-29 00:57:07 -0500

    blackjack game

--- BlackJack/Blackjack.c
+++ BlackJack/Blackjack.c
@@ -50,8 +50,8 @@
 	    while(1) {
 	    	char in;
 	    	scanf ("%c ", &in);						//asks if player wants to play
-	    		if(in =='y') {									//player hits y to starts to game
-	    		int ThirdPick_P = RandomNumbers(); 				//Picking the third card of player
+	    		if(in =='y') {						//player hits y to starts to game
+	    		int ThirdPick_P = RandomNumbers(); 			//Picking the third card of player
 	    		int ThirdPick_D = RandomNumbers();				//picking third card of the dealer
 	    		D_Picks = D_Picks+ThirdPick_D;					//Iterating the Card picks of the Dealer
 	    		P_Picks = P_Picks+ThirdPick_P;					//Iterating the card picks of the player
