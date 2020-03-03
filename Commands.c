f47ea8ca488f02f87d3a24c80c4dd7daa5123c35
Srikanth Pendem <55446776+srikanthpendem@users.noreply.github.com>
2020-03-03 15:04:12 -0500

    linux commands

--- Commands.c
+++ Commands.c
@@ -9,4 +9,8 @@
 >> whereis file     //to know the location of the file.
 
 >> cat > ../path/..filename.format  // to create a document file and then can be written.
+>> nano ../path/.. filename.format  // to create a document file and then can be written.
+
+>> gcc filename.c -o filename     //to compile a C file
+>> ./filename                     //to execute the compiled C file
  
