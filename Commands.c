c584513b11e19e4c7640f67c22cb72445511720b
Srikanth Pendem <55446776+srikanthpendem@users.noreply.github.com>
2020-03-03 14:55:49 -0500

    Linux commands

--- Commands.c
+++ Commands.c
@@ -1,4 +1,8 @@
 >> sudo dpkg --configure -a   //checks what dependencies are required
 >> sudo apt-get install -f   //installs the listed missing dependencies
+ 
 >> sudo ln -s ../path/../executable_file /usr/local/bin  //To create an application shortcut.
+
+>> sudo chmod +x ../path/executable.bin    //to change .bin to executable to further Line 5
+>> sudo dpkg -i ../path/executable.bin    //to install the .deb file after Line 4 execution
  
