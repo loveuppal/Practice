57fb58f98807d4a6b536543ae2941e3ea061c9ef
Srikanth Pendem <55446776+srikanthpendem@users.noreply.github.com>
2020-03-11 17:44:12 -0400

    Update printtwonumbers.c

--- PthreadBasic/printtwonumbers.c
+++ PthreadBasic/printtwonumbers.c
@@ -2,20 +2,20 @@
 #include<pthread.h>
 
 int *threadFunction( int *v) {
-	*v = 25;								//  to print this value(function)
+	*v = 25;						//  to print this value(function)
 	return 0;
 }
 
 int main() {
 	
-	int y = 19;								// also to print this value(main)
-	printf("%d ", y);							// prints value(main)
+	int y = 19;						// also to print this value(main)
+	printf("%d ", y);					// prints value(main)
 
-	pthread_t id;								// create a id
-	pthread_create (&id, NULL, &threadFunction, &y);			// joining id with the function
+	pthread_t id;						// create a id
+	pthread_create (&id, NULL, &threadFunction, &y);	// joining id with the function
 	
-	void *result;								// creates a result
-	pthread_join (id, &result);						// joins id with result
+	void *result;						// creates a result
+	pthread_join (id, &result);				// joins id with result
 	
-	printf(" %d",y );							//prints value(function)
+	printf(" %d",y );					//prints value(function)
 }
