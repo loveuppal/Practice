588929e8a1dbd33ffdaa755c008f551ae9a2faaf
Srikanth Pendem <55446776+srikanthpendem@users.noreply.github.com>
2020-03-12 01:34:00 -0400

    Guess game using binary search

--- BinarySearch/binarysearch.c
+++ BinarySearch/binarysearch.c
@@ -9,15 +9,21 @@ int binarySearch(int x)
 	for(int i = 0; i <= 1000; i++){
 	    arr[i] = i;					//creating an array containing 1 to 1000 numbers
 	}
+	
     while (first <= last) {
-        int middle = first + (last - first) / 2;	//calculating mid index of the array
-        if (arr[middle] == x)				//if guessed number = mid index
+       
+	    int middle = first + (last - first) / 2;	//calculating mid index of the array
+       
+	    if (arr[middle] == x)			//if guessed number = mid index
             return win();				//returns to win
-        if (arr[middle] < x)
+       
+	    if (arr[middle] < x)
             first = middle + 1;				//finds new mid index if guessed number is less than previous mid index
-        else
+       
+	    else
             last = middle - 1;				//finds new mid index if guessed number is greater than previous mid index
     }
+	
     printf("Out Reach");				//returns if guessed number is out of limits
     return 0;
 }
