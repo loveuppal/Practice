3140ff683f3087e91bc7582bd0bec77345e0746d
Srikanth Pendem <srikanth.pendem25@gmail.com>
2020-04-30 18:18:33 -0400

    Update

--- LinuxCommands/Commands.c
+++ LinuxCommands/Commands.c
@@ -1,50 +1,48 @@
-<<<<<<< HEAD
 >> sudo dpkg --configure -a					//checks what dependencies are required
 >> sudo apt-get install -f					//installs the listed missing dependencies
  
 >> sudo ln -s ../path/../executable_file /usr/local/bin		//To create an application shortcut.
 
->> sudo chmod +x ../path/executable.bin				//to change .bin to executable to further Line 6.
->> sudo dpkg -i ../path/executable.bin				//to install the .deb file after Line 5 execution.
-=======
->> sudo dpkg --configure -a    //checks what dependencies are required
->> sudo apt-get install -f  		//installs the listed missing dependencies
+>> sudo chmod +x ../path/executable.bin				//to change .bin to executable
+>> sudo dpkg -i ../path/executable.bin				//to install the .deb file
+
+>> sudo dpkg --configure -a    					//checks what dependencies are required
+>> sudo apt-get install -f  					//installs the listed missing dependencies
  
 >> sudo ln -s ../path/../executable_file /usr/local/bin		//To create an application shortcut.
 
->> sudo chmod +x ../path/executable.bin    //to change .bin to executable to further Line 6.
->> sudo dpkg -i ../path/executable.bin    	//to install the .deb file after Line 5 execution.
->>>>>>> 5897c55cf494ccdb4f364534711853affa2f155c
+>> sudo chown $USER -R /targetPath	 			//To change the ownership of the device
 
->> sudo chown $USER -R /targetPath	 	//To change the ownership of the device
-
->> whereis file     				//to know the location or the path of the file.
+>> whereis file     						//to know the location or the path of the file.
 >> file filename 						// To know the file type
 
 >> sudo mount /dev/mountFile ../path/..     			//mount a device.
->> sudo umount /dev/mountFile               //dismount a device
->> lsblk                                  // lists the devices.
->> ls -l ../path/file                    //to know ownsership
+>> sudo umount /dev/mountFile               			//dismount a device
+>> lsblk                                  			// lists the devices.
+>> ls -l ../path/file                    			//to know ownsership
  
->> sudo su      //Get into the root
+>> sudo su      						//Get into the root
 >> Ctrl+D (multiple times until exit) 				// to exit from root
->> su username                           // to exit from root
+>> su username                           			// to exit from root
 
->> cat > ../path/..filename.format  // to create a document file and then can be written.
->> nano ../path/.. filename.format  // to create a document file and then can be written.
+>> cat > ../path/..filename.format  				// to create a document file and then can be written.
+>> nano ../path/.. filename.format  				// to create a document file and then can be written.
 
->> gcc filename.c -o filename     //to compile a C file
->> ./filename                     //to execute the compiled C file
+>> gcc filename.c -o filename     				//to compile a C file
+>> ./filename                     				//to execute the compiled C file
 
->>tar xvzf filename.tar.gz        //to extract .gz file
->>tar xf filename.tar.xz          //to extract .xz file
+>>tar xvzf filename.tar.gz       				//to extract .gz file
+>>tar xf filename.tar.xz        				//to extract .xz file
+
+>> sudo mkfs.FILEFORMAT /dev/path     				//to change filesystem of drives
+>> sudo fdisk -l                     				//to check the drives
 
 /*always move manual installation package into /usr/local*/
 >> sudo mv ../path/filefolder /usr/local.
  
  /*to change folders containing multiple sub-folders of files use -r (recursive)*/
- >> cp -r    //copy folder containg multile files
- >> rm -r    //delete folders containg multiple files
+ >> cp -r    							//copy folder containg multile files
+ >> rm -r    							//delete folders containg multiple files
  
  /*If error like Glob, regex, unable to locate. do not apt install, instead use*/
  >> sudo dpkg -i executablefile
@@ -52,5 +50,4 @@
  /*If left or right mouse button doesnt work*/
  >> sudo apt install gnome-tweaks >> select "Area"
  
- >> sudo mkfs.FILEFORMAT /dev/path     //to change filesystem of drives
- >> sudo fdisk -l                     //to check the drives
+
