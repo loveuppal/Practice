812ff10b07fab71655c72659f415e5ee5dfcc380
Srikanth Pendem <55446776+srikanthpendem@users.noreply.github.com>
2020-06-29 15:01:58 -0400

    Update Commands.c

--- LinuxCommands/Commands.c
+++ LinuxCommands/Commands.c
@@ -5,9 +5,6 @@
 
 >> sudo chmod +x ../path/executable.bin				//to change .bin to executable
 >> sudo dpkg -i ../path/executable.bin				//to install the .deb file
-
->> sudo dpkg --configure -a    					//checks what dependencies are required
->> sudo apt-get install -f  					//installs the listed missing dependencies
  
 >> sudo ln -s ../path/../executable_file /usr/local/bin		//To create an application shortcut.
 
